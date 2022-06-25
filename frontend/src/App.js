c6361215c485fe9073de5e6739cc819828186999
shagors <shajjadshagor@gmail.com>
2022-06-25 23:14:00 +0600

    set header and footer design

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import { Container } from 'react-bootstrap'
+import Footer from './components/Footer';
+import Header from './components/Header';
 function App() {
   return (
     <>
-      <h1>Welcome To ProShop</h1>
+      <Header></Header>
+      <main className='py-3'>
+        <Container>
+          <h1>Welcome To ProShop</h1>
+        </Container>
+      </main>
+      <Footer></Footer>
     </>
   );
 }
