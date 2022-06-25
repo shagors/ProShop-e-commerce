ba539bc974e10a4e1d878e0cbdf784e8936d32ef
shagors <shajjadshagor@gmail.com>
2022-06-26 00:24:26 +0600

    add body add and shows products on display and design it

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { Container } from 'react-bootstrap'
 import Footer from './components/Footer';
 import Header from './components/Header';
+import HomeScreen from './screens/HomeScreen';
 function App() {
   return (
     <>
       <Header></Header>
       <main className='py-3'>
         <Container>
-          <h1>Welcome To ProShop</h1>
+          <HomeScreen></HomeScreen>
         </Container>
       </main>
       <Footer></Footer>
