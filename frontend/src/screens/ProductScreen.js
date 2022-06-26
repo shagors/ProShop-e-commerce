685ef9eb2cebdb822c2c938cccf7361b290ea736
shagors <shajjadshagor@gmail.com>
2022-06-26 16:05:49 +0600

    products details set another page and display set but some error find it

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,8 +1,68 @@
 import React from 'react'
+import { Button, Card, Col, Image, ListGroup, ListGroupItem, Row } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import Rating from '../components/Rating'
+import products from '../products'
 
-const ProductScreen = () => {
+const ProductScreen = ({ match }) => {
+  const product = products.find((p) => p._id === match.params.id);
   return (
-    <div>ProductScreen</div>
+    <>
+      <Link className='btn btn-light my-3' to='/'>Go Back</Link>
+      <Row>
+        <Col md={6}>
+            <Image src={product.image} alt={product.name} fluid/>
+        </Col>
+        <Col md={3}>
+            <ListGroup variant='flush'>
+              <ListGroupItem>
+                  <h2>{product.name}</h2>
+              </ListGroupItem>
+              <ListGroupItem>
+                  <Rating value={product.rating} text={`${product.numReviews} reviews`}></Rating>
+              </ListGroupItem>
+              <ListGroupItem>
+                Price: ${product.price}
+              </ListGroupItem>
+              <ListGroupItem>
+                Description: {product.description}
+              </ListGroupItem>
+            </ListGroup>
+        </Col>
+        <Col md={3}>
+            <Card>
+                <ListGroup variant='flush'>
+                    <ListGroupItem>
+                        <Row>
+                            <Col>
+                              Price: 
+                            </Col>
+                            <Col>
+                              <strong>${product.price}</strong>
+                            </Col>
+                        </Row>
+                    </ListGroupItem>
+
+                    <ListGroupItem>
+                        <Row>
+                            <Col>
+                              Status: 
+                            </Col>
+                            <Col>
+                              {product.countInStock > 0 ? 'In Stock' : 'Out of Stock'}
+                            </Col>
+                        </Row>
+                    </ListGroupItem>
+                    <ListGroupItem>
+                      <Button className='btn-block' type='button' disabled={product.countInStock === 0 }>
+                        Add To Cart
+                      </Button>
+                    </ListGroupItem>
+                </ListGroup>
+            </Card>
+        </Col>
+      </Row>
+    </>
   )
 }
 
