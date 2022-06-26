9f0a4a533e82dac56b66df951d1b717e7f94f589
shagors <shajjadshagor@gmail.com>
2022-06-26 14:18:27 +0600

    add many files router bootstrap and design home screen and set responsive and rating set

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,20 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom'
 import { Container } from 'react-bootstrap'
 import Footer from './components/Footer';
 import Header from './components/Header';
 import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 function App() {
   return (
     <>
       <Header></Header>
       <main className='py-3'>
         <Container>
-          <HomeScreen></HomeScreen>
+          <Routes>
+            <Route path='/' element={<HomeScreen></HomeScreen>}></Route>
+            <Route path='/product/:id' element={<ProductScreen></ProductScreen>}></Route>
+          </Routes>
         </Container>
       </main>
       <Footer></Footer>
