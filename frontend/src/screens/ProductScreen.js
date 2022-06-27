5587636fc7973dad313e4a8bb04fb47c693eace0
shagors <shajjadshagor@gmail.com>
2022-06-27 16:30:36 +0600

    data fetch from server and product details bug fix

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,17 +1,27 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
 import { Button, Card, Col, Image, ListGroup, ListGroupItem, Row } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import Rating from '../components/Rating';
-import products from '../products'
 
-const ProductScreen = ({ match }) => {
-  const product = products.find(p => p._id === match.params.id);
+const ProductScreen = () => {
+  const {id} = useParams();
+  const [product, setProduct] = useState({});
+
+  useEffect( () => {
+        const fetchProduct = async () => {
+            const { data } = await axios.get(`/api/products/${id}`);
+            setProduct(data);
+        }
+        fetchProduct();
+    } , []);
+
   return (
     <>
       <Link className='btn btn-light my-3' to='/'>Go Back</Link>
       <Row>
         <Col md={6}>
-            <Image src={product.image} alt={product.name} fluid/>
+            <Image src={product?.image} alt={product.name} fluid/>
         </Col>
         <Col md={3}>
             <ListGroup variant='flush'>
