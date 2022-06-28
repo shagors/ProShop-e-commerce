f5b76a5dacf3d42d77207892ca73711a90614092
shagors <shajjadshagor@gmail.com>
2022-06-28 08:02:50 +0600

    set mongoose database schema and set all fields

---
+++ backend/models/productModel.js
@@ -0,0 +1,65 @@
+import mongoose from "mongoose";
+
+const reviewScheme = mongoose.Schema({
+    name: { type: String, required: true },
+    rating: { type: Number, required: true },
+    comment: { type: String, required: true },
+}, {
+    timestamps: true
+});
+
+const productScheme = mongoose.Schema(
+{
+    user:{
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        red: 'User',
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true,
+    },
+    brand: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    reviews: [reviewScheme],
+    rating: {
+        type: Number,
+        required: true,
+        default: 0,
+    },
+    numReviews: {
+        type: Number,
+        required: true,
+        default: 0
+    },
+    price: {
+        type: Number,
+        required: true,
+        default: 0
+    },
+    countInStock: {
+        type: Number,
+        required: true,
+        default: 0
+    },
+}, {
+    timestamps: true
+});
+
+const Product = mongoose.model('Product', productScheme);
+
+export default Product;
