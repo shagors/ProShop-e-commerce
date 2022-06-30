19f759d11c030d3b03f3e21e017a1653aea8be4a
shagors <shajjadshagor@gmail.com>
2022-06-30 07:37:52 +0600

    redux install and redux call and set up redux for use and call it from index.js file

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import store from './store';
 import { BrowserRouter } from "react-router-dom";
 import './bootstrap.min.css'
 import './index.css';
@@ -8,11 +10,11 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
