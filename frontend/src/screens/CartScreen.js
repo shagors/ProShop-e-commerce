259e8a02ccfacbb8810ea9bbd6d69ce6ad7f17f8
shagors <shajjadshagor@gmail.com>
2022-07-01 15:19:10 +0600

    data cart add and display log and check file and save local storage and save the cart daia

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link, useLocation, useNavigate, useParams } from 'react-router-dom';
+import Message from '../components/Message';
+import { addToCart } from '../actions/cartActions'
 
 const CartScreen = () => {
+  const { id } = useParams();
+  const location = useLocation();
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const cart = useSelector(state => state.cart);
+  const { cartItems } = cart;
+
+  console.log(cartItems);
+
+  const qty = location.search ? Number(location.search.split('=')[1]) : 1;
+
+  useEffect( () => {
+    if(id){
+      dispatch(addToCart(id, qty))
+    }
+  } , [dispatch, id, qty]);
+
+
   return (
     <div>CartScreen</div>
   )
 }
 
-export default CartScreen
+export default CartScreen;
