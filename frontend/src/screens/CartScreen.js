371b905e66998b3167034bb0b48d023387826f54
shagors <shajjadshagor@gmail.com>
2022-07-20 20:12:51 +0600

    cart page add and data show and date set and display

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { Link, useLocation, useNavigate, useParams } from 'react-router-dom';
 import Message from '../components/Message';
 import { addToCart } from '../actions/cartActions'
+import { Button, Col, Form, Image, ListGroup, ListGroupItem, Row } from 'react-bootstrap';
 
 const CartScreen = () => {
   const { id } = useParams();
@@ -23,9 +24,58 @@ const CartScreen = () => {
     }
   } , [dispatch, id, qty]);
 
+  const removeFromCartHandler = (id) => {
+    console.log('remove');
+  }
+
 
   return (
-    <div>CartScreen</div>
+    <Row>
+      <Col md={8}>
+        <h1>Shopping Cart</h1>
+        {cartItems.length === 0 ? <Message>Your cart is empty <Link to='/'>Go Back</Link> </Message> : (
+          <ListGroup variant='flush'>
+            {
+              cartItems.map(item => (
+                <ListGroupItem key={item.product}>
+                  <Row>
+                    <Col md={2}>
+                      <Image src={item.image} alt={item.name} fluid rounded />
+                    </Col>
+                    <Col md={3}>
+                      <Link to={`/product/${item.product}`}>{item.name}</Link>
+                    </Col>
+                    <Col md={2}>${item.price}</Col>
+                    <Col md={2}>
+                      <Form.Control as='select' value={qty} onChange={(e) => dispatch(addToCart(item.product, Number(e.target.value)))}>
+                          {
+                            [...Array(item.countInStock).keys()].map(x => (
+                              <option key={x + 1} value={x + 1}>
+                                {x + 1}
+                              </option>
+                            ))
+                          }
+                      </Form.Control>
+                    </Col>
+                    <Col md={2}>
+                      <Button type='button' variant='light' onClick={() => removeFromCartHandler(item.product)}>
+                        <i className='fas fa-trash'></i>
+                      </Button>
+                    </Col>
+                  </Row>
+                </ListGroupItem>
+              ))
+            }
+          </ListGroup>
+        )}
+      </Col>
+      <Col md={2}>
+        
+      </Col>
+      <Col md={2}>
+
+      </Col>
+    </Row>
   )
 }
 
