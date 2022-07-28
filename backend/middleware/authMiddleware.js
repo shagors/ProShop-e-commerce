794c1c7955dccaac435cc02e72a007ae691d4152
shagors <shajjadshagor@gmail.com>
2022-07-29 01:48:14 +0600

    user add and password set hash and create user check if already then send exists message. password send hash and hash passsword send in mongo

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,12 +1,31 @@
 import jwt from 'jsonwebtoken';
+import expressAsyncHandler from 'express-async-handler';
 import User from '../models/userModel.js';
 
-const protect = async (req, res, next) => {
+const protect = expressAsyncHandler(async (req, res, next) => {
     let token;
 
-    console.log(req.headers.authorization);
+    if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')){
+        try {
+            token = req.headers.authorization.split(' ')[1];
+            const decoded = jwt.verify(token, process.env.JWT_TOKEN);
+
+            req.user = await User.findById(decoded.id).select('-password');
+
+            next();
+        } catch (error) {
+            console.error(error);
+            req.status(401)
+            throw new Error('Not authorized, token failed');
+        }
+    }
+
+    if(!token){
+        res.status(401)
+        throw new Error('Not authorized, no token')
+    }
     
     next();
-};
+});
 
 export {protect};
